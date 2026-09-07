e882f022d5c8ec4250520e2e6c0fd34ce3cadfa3
Nathan Zhao <nathan.ze.zhao@gmail.com>
2026-09-07 15:10:05 -0400

    vercel analytics

--- src/bits-app.jsx
+++ src/bits-app.jsx
@@ -1,5 +1,13 @@
 import { createRoot } from 'react-dom/client';
 import './screen.css';
 import App from './WheelPage.jsx';
+import { Analytics } from '@vercel/analytics/react';
+
+createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+    <Analytics />
+  </>
+);
 
 createRoot(document.getElementById('root')).render(<App />);
